b6a0fd448041191acd3f0d83155c8e5cfd86cf6d
Dawid <dawid_03@onet.eu>
2025-03-28 00:07:09 +0100

    initial client app

---
+++ app/components/UniversalButton.js
@@ -0,0 +1,31 @@
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+const UniversalButton = ({ text, click, color, textColor = "white", width, height, font, insertInside }) => {
+
+    const styles = StyleSheet.create({
+        button: {
+            display: "flex",
+            backgroundColor: color,
+            justifyContent: "center",
+            alignSelf: 'center',
+            padding: 12,
+            borderRadius: 100,
+            alignItems: "center"
+        }
+    })
+
+    if (insertInside)
+        return (
+            <TouchableOpacity onPress={click ? () => { click() } : () => { }} style={[styles.button, width ? { width: width } : {}, height ? { height: height } : {}]}>
+                {insertInside}
+            </TouchableOpacity>
+        )
+    else
+        return (
+            <TouchableOpacity onPress={click ? () => { click() } : () => { }} style={[styles.button, width ? { width: width } : {}, height ? { height: height } : {}]}>
+                <Text style={[{ color: textColor, textAlign: "center" }, font ? { fontSize: font } : {}]}>{text}</Text>
+            </TouchableOpacity>
+        )
+
+}
+export default UniversalButton
