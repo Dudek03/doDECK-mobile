1d86feebdf4e3aa8458a63d4208b7b9438df238a
Dudek03 <dawid_03@onet.eu>
2026-03-02 15:25:25 +0100

    add live action data support

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native'
 import axios from 'axios'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import VolumeControl from './components/VolumeControl'
 import { useRouter } from "expo-router";
 
@@ -33,7 +32,14 @@ const INITIAL_BUTTONS = [
         color: '#2ecc71',
         type: 'WIDGET',
         payload: { command: 'open_mixer' }
-    }
+    },
+    {
+        id: '5',
+        title: 'cpu usage',
+        color: '#5865F2',
+        type: 'LIVE ACTION',
+        payload: { sensor: 'cpu' }
+    },
 ]
 
 
@@ -44,9 +50,27 @@ const MainScreen = () => {
     const [isAudioChanging, setIsAudioChanging] = useState(false)
     const [appsAudio, setAppsAudio] = useState({})
     const [serverIP, setServerIP] = useState('')
+    const [hardwareData, setHardwareData] = useState({ 'cpu': 0, 'ram': 0 })
 
 
     const BASE_URL = `http://${serverIP}:5000`
+
+
+    useEffect(() => {
+        let isLiveData = buttons.some(btn => btn.type == "LIVE ACTION")
+        if (!isLiveData || !serverIP) return
+
+        const RESTInterval = setInterval(async () => {
+            try {
+                const res = await axios.get(`http://${BASE_URL}:5000/system/getLiveData`);
+                if (res.data && res.data.length > 0) {
+                    setHardwareData(res.data)
+                }
+            } catch (e) {
+                console.error("nie udalo sie pobrac danych", e);
+            }
+        }, 3000)
+    }, [serverIP, buttons])
 
     useEffect(() => {
         const appInit = async () => {
@@ -99,15 +123,49 @@ const MainScreen = () => {
         }
     }
 
-    const renderItem = ({ item }) => (
-        <TouchableOpacity
-            style={[styles.gridItem, { backgroundColor: item.color }]}
-            onPress={() => handleButtonPress(item)}
-            activeOpacity={0.7}
-        >
+    const ActionTile = ({ item, onPress }) => (
+        <TouchableOpacity style={[styles.gridItem, { backgroundColor: item.color }]} onPress={onPress}>
             <Text style={styles.gridItemText}>{item.title}</Text>
         </TouchableOpacity>
-    )
+    );
+
+    const SystemTile = ({ item, value }) => (
+        <View style={[styles.gridItem, { backgroundColor: item.color }]}>
+            <Text style={styles.gridItemText}>{item.title}</Text>
+            <Text style={styles.largeValueText}>{value}%</Text>
+        </View>
+    );
+
+    const YouTubeTile = ({ item, subs }) => (
+        <View style={[styles.gridItem, { backgroundColor: item.color }]}>
+            <Icon name="youtube" size={30} color="white" />
+            <Text style={styles.gridItemText}>Widzowie</Text>
+            <Text style={styles.largeValueText}>{subs > 0 ? subs : "..."}</Text>
+        </View>
+    );
+
+    const renderItem = ({ item }) => {
+        if (item.type === 'ACTION') {
+            return <ActionTile item={item} onPress={() => handleButtonPress(item)} />;
+        }
+
+        if (item.type === 'LIVE_DATA') {
+            const sensor = item.payload.sensor;
+
+            switch (sensor) {
+                case 'cpu':
+                    return <SystemTile item={item} value={hardwareData.cpu} />;
+                case 'ram':
+                    return <SystemTile item={item} value={hardwareData.ram} />;
+                /* case 'youtube_subs':
+                       return <YouTubeTile item={item} subs={systemStats.youtube_subs} />; */
+                default:
+                    return <View style={styles.gridItem}><Text>Brak danych</Text></View>;
+            }
+        }
+
+        if (item.type === 'WIDGET') { }
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -139,7 +197,7 @@ const MainScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#121212', // Nowoczesny, bardzo ciemny szary
+        backgroundColor: '#121212',
     },
     header: {
         height: 80,
