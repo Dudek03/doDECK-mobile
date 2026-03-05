d1302a3b0600c9bab5b1737487b6a9b984fa59b4
Dawid <dawid_03@onet.eu>
2026-03-05 13:34:07 +0100

    delete screen title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Ekran główny" }} />
-      <Stack.Screen name="SettingsScreen" options={{ title: "Ustawienia" }} />
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="SettingsScreen" options={{ headerShown: false }} />
+        </Stack>
+    );
 }
 
